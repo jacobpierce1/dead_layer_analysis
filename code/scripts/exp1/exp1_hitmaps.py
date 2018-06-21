b2dd191af090f4a107c383c7bae44c74d6abd43c
Jacob <jacobpierce@uchicago.edu>
2018-06-20 22:27:22 -0500

    staging commits for git commit history fix

--- code/scripts/exp1/exp1_hitmaps.py
+++ code/scripts/exp1/exp1_hitmaps.py
@@ -14,17 +14,20 @@ import bpt
 import scipy.optimize
 from scipy.stats import chi2
 import os 
-
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+import matplotlib.cm
 
 # config
 # use_variable_thickness = 1
-mesh_size = 5 
+mesh_size = 4
 
 
                    
 
-num_basis_functions = mesh_size + 1 
+num_basis_functions = mesh_size
 
+
+integrals = None 
 
 
 
@@ -61,6 +64,8 @@ def primary_peak_detector( peaks_detected, histo ) :
                                            peak0 + next_peak_offsets[i] + 15 ] ) )
     
     return ret 
+
+
 
 
 
@@ -151,24 +156,61 @@ def point_source_resid( params, hitmap, d_hitmap ) :
 
 def compute_basis_function_integrals( first_displacement, nhat, max_radius, mesh_size,
                                       save_path = None  ) :
+    
+    # if save_path is not None : 
+    #     if os.path.exists( save_path ) :
+    #         pass
+    if np.allclose( first_displacement, compute_basis_function_integrals.first_displacement_prev,
+                    1.0e-3 ) : 
+        # print( 'INFO: recycling integrals...' ) 
+        return compute_basis_function_integrals.integrals_prev
 
-    if save_path is not None : 
-        if os.path.exists( save_path ) :
-            pass
+    compute_basis_function_integrals.first_displacement_prev = first_displacement
     
+    # print( 'INFO: computing integrals...' )
     
-    print( 'INFO: computing integrals...' )
-    
-    mesh = np.linspace( 0, max_radius, mesh_size ) 
+    mesh = np.linspace( 0.0, max_radius, mesh_size ) 
 
+    num_basis_functions = len( mesh )
+    
+    # consrtuct supports
+    
+    supports = np.zeros( ( num_basis_functions, 2 ) )
+
+    # print( num_basis_functions )
+    
+    for i in range( num_basis_functions  ) :
+
+        # print( i ) 
+
+        if i == 0 :
+            supports[i] = np.array( [ mesh[0], mesh[1] ] )
+
+        elif i == num_basis_functions - 1 :
+            supports[i] = np.array( [ mesh[-2], mesh[-1] ] )
+
+        else :
+            supports[i] = np.array( [ mesh[i-1], mesh[i+1] ] )
+
+    # print( supports ) 
+
+
+    basis_functions = [ lambda x : P1_basis_function( mesh[i], mesh[1], x )
+                        for i in range( num_basis_functions ) ]
+    
+        
     def integrand( r, phi, d, i ) :
-        rprime = np.array( [ r * np.cos( phi ), r * np.sin( phi ), 0 ]  )
+        # print( k ) 
+        rprime = np.array( [ r * np.cos( phi ), r * np.sin( phi ), 0.0 ]  )
         tmp = ( d - rprime ) 
-        return ( r * np.cos( phi ) * P1_finite_element_basis( mesh, i, x )
-                 * ( tmp ).dot( nhat ) / np.linalg.norm( tmp ) ** 3  )
-
+        return  ( r * np.abs( basis_functions[i]( r )
+                              * ( tmp ).dot( nhat )
+                              / np.linalg.norm( tmp ) ** 3.0 )  )
+        # print( ret )
+        # return ret 
+        
     
-    ret = np.zeros( ( len(mesh) + 1, 32, 32 ) )
+    ret = np.zeros( ( 32, 32, num_basis_functions ) )
 
     for x in range(32) :
         for y in range(32) :
@@ -176,18 +218,43 @@ def compute_basis_function_integrals( first_displacement, nhat, max_radius, mesh
             # this depends on nhat 
             d = first_displacement + 2 * np.array( [ -y, x, 0 ] ) 
                     
-            for k in range( len( mesh ) + 1 ) : 
+            for k in range( num_basis_functions ) : 
                 integral = scipy.integrate.dblquad(
-                    integrand, 0.0, 2*np.pi, 0.0, max_radius * 1.0,
-                    args = (d,i) )
+                    integrand, 0.0, 2 * np.pi, * supports[k],
+                    args = (d,k),
+                    epsabs = 1e-11, epsrel = 1e-11 )
 
-                ret[k,i,j] = integral[0]
+                ret[x,y,k] = integral[0]
+                
+                # if integral[0] == 0 :
+                #     print( 'int is 0' )
+                #     print( i )
+                #     print( j )
+                #     print( k ) 
+                
+                # print( integral[0] ) 
+                # print( ret[i,j,k] ) 
+                
+    # print( 'Done.' )
 
-    print( 'Done.' )
+    compute_basis_function_integrals.integrals_prev = ret 
     
     return ret 
+compute_basis_function_integrals.first_displacement_prev = np.zeros( 3 )  
+compute_basis_function_integrals.integrals_prev = None
 
 
+def P1_basis_function( center, mesh_space, x ) :
+
+    if x > center :
+        return ( center + mesh_space - x ) / mesh_space
+
+    else :
+        return ( x - ( center - mesh_space ) ) / mesh_space 
+
+
+    
+        
 
 # def indicator( x1, x2, x ) :
 
@@ -225,35 +292,37 @@ def compute_basis_function_integrals( first_displacement, nhat, max_radius, mesh
     
                             
                              
-def P1_finite_element_basis( mesh, i, x ) :
+# def P1_finite_element_basis( mesh, i, x ) :
 
-    if i == 0 :
+#     if i == 0 :
 
-        if x > mesh[0] and x < mesh[1] :
-            return -x / mesh[1]
+#         if x > mesh[0] and x < mesh[1] :
+#             return ( mesh[1] - x ) / mesh[1]
 
-        else :
-            return 0
+#         else :
+#             return 0
 
-    elif i == len( mesh ) :
+#     elif i == len( mesh ) :
 
-        if x > mesh[-2] and x < mesh[-1] :
-            return ( x - mesh[-1] ) / mesh[1]
+#         if x > mesh[-2] and x < mesh[-1] :
+#             return ( x - mesh[-1] ) / mesh[1]
 
-        else :
-            return 0
+#         else :
+#             return 0
 
-    else :
+#     else :
 
-        if x > mesh[i-1] and x <= mesh[i] :
-            return ( x - mesh[i-1] ) / mesh[1]
+#         if x > mesh[i-1] and x <= mesh[i] :
+#             return ( x - mesh[i-1] ) / mesh[1]
 
-        elif x > mesh[i] and x < mesh[i+1] :
-            return ( mesh[i+1] - x ) / mesh[1]
+#         elif x > mesh[i] and x < mesh[i+1] :
+#             return ( mesh[i+1] - x ) / mesh[1]
 
-        else :
-            return 0 
+#         else :
+#             return 0
 
+#     print( 'ERROR: should not be reached.' )
+#     raise( IndexError ) 
     
 
 
@@ -262,23 +331,35 @@ def P1_finite_element_basis( mesh, i, x ) :
 def variable_thickness_source_counts( params, hitmap, max_radius, mesh_size ) :
 
     
-    nhat = np.array( [0,0,1]  )
+    nhat = np.array( [ 0, 0, 1.0]  )
     
-    # A = params[0]
+    # print( params )
+    # print( params.dtype ) 
+    
     first_displacement = params[0:3 ]
-    A = params[ 3 : num_basis_functions ]
 
+    # restrict the source thicknesses to be positive by
+    # applying positive injective function 
+    A = np.exp( params[ 3: ] )
+    # A = params[ 3: ] 
+    
     integrals = compute_basis_function_integrals( first_displacement, nhat,
                                                   max_radius, mesh_size )
 
-    counts = np.zeros( (32,32) ) 
-    
-    for x in range(32) :
-        for y in range( 32 ) :
-            displacement = 2.0 * np.array( [ -y, x, 0 ] ) + first_displacement 
-            costheta = displacement.dot( nhat ) / np.linalg.norm( displacement )
-            counts[x,y] = A.dot( integrals ) 
+    # print( integrals.dtype ) 
 
+
+    # print( integrals ) 
+    # print( np.max(integrals ) )
+    # print( np.min( integrals ) ) 
+    
+    counts = integrals.dot( A ) 
+
+    # print( counts )
+    # print( np.max( counts ) )
+
+    # print( counts.shape ) 
+    
     return counts 
 
 
@@ -290,14 +371,27 @@ def variable_thickness_source_counts( params, hitmap, max_radius, mesh_size ) :
 
 def variable_thickness_resid( params, hitmap, d_hitmap, max_radius, mesh_size ) :
 
+    # print( params )
+    # print( hitmap )
+    # print( d_hitmap )
+    # print( max_radius )
+    # print( mesh_size ) 
+    
     counts = variable_thickness_source_counts( params, hitmap, max_radius, mesh_size ) 
 
     resid = ( counts - hitmap ) / d_hitmap 
     
     resid[ np.isnan(resid) ] = 0
 
-    return resid.flatten()
+    # print( hitmap )
+    # print( counts ) 
+    # print( resid ) 
 
+    ret = resid.flatten()
+
+    # print( ret.dot( ret ) )
+
+    return ret
 
 
 
@@ -318,83 +412,190 @@ hitmaps = spec.compute_hitmaps( (32,32), data_retriever,
                                 plot = 0,
                                 reset = 0,
                                 filter_data = 1,
-                                # debug_coords = ( 20, 20 ),
+                                debug_coords = ( 20, 20 ),
                                 rel_plot_bounds = [-100, 120] ) 
 
 
 
 
 
+# f, axarr = plt.subplots( 3, 3, figsize = ( 10, 8 ) ) 
+
+
+# params_guesses = [ np.array( [ [ 3.0e7, 87.10, -9.31, 58.35 ],
+#                                [ 3.0e7, 32.95, -31.45, 57.88 ],
+#                                [ 3.0e7, 85.35, -46.06, 57.74 ] ] ),
+#                    np.array( [ [ 87.10, -9.31, 58.35  ] + [-1.0] * num_basis_functions,
+#                                [ 32.95, -31.45, 57.88 ] + [-1.0] * num_basis_functions,
+#                                [ 85.35, -46.06, 57.74 ] + [-1.0] * num_basis_functions ] ) ]
+                   
+# eps = [ None, 10.0 ] 
+
+
+# for i in range( 3 ) :
+
+#     fitfuncs = [ point_source_resid, variable_thickness_resid ]
+#     names = [ 'point source calibration', 'variable thickness calibration' ] 
+
+#     filter_data( hitmaps[i] )
+#     remove_strips( hitmaps[i], [0,31,10,12,27,15,3], [0,31,15,16,21] ) 
+    
+#     axarr[0,i].imshow( hitmaps[i] ) 
+
+
+#     hitmap = hitmaps[i]
+#     d_hitmap = np.sqrt( hitmaps[i] )
+#     d_hitmap[ d_hitmap == 0 ] = 1
+
+#     args = [ ( hitmap, d_hitmap ),
+#              ( hitmap, d_hitmap, 1.0, mesh_size ) ]
+    
+
+#     for j in [0] : 
+#     # for j in range(2) :
+
+#         print( names[j].upper() )
+                                 
+#         params_guess = np.copy( params_guesses[j][i] )
+        
+#         ret = scipy.optimize.leastsq( fitfuncs[j], params_guess,
+#                                       full_output = 1, args = args[j] )
+        
+#         # else :
+#         #     ret = scipy.optimize.least_squares( fitfuncs[j], params_guess,
+#         #                                         args = args[j], x_scale = [ 50., 50., 50. ]
+#         #                                         + [ 15 ] * num_basis_functions )
+
+#         result, cov, info, mesg, success = ret
+#         success = success >= 1
+
+
+#         chisqr = np.sum( info['fvec']**2 )
+#         ndata = np.count_nonzero( hitmaps[i][ ( hitmaps[i] != np.nan ) ] ) 
+#         nfree =  ndata - len( result ) 
+#         redchisqr = chisqr / nfree
+
+
+#         print( result )
+#         print( redchisqr )
+#         pvalue = 1 - chi2.cdf( chisqr, nfree )
+#         print( 'pvalue: %.2f' % pvalue )
+#         print( success )
+
+
+#         if cov is not None :
+#             errors = np.sqrt( np.diag( cov ) * redchisqr )
+#             print( errors ) 
+
+#         print( '\n\n' ) 
+
+#         if j == 0 : 
+#             resid = ( hitmap - point_source_counts( result, hitmap ) ) / d_hitmap 
+
+#         else:
+#             resid = ( hitmap - variable_thickness_source_counts( result, hitmap,
+#                                                                  1.0, mesh_size ) ) / d_hitmap 
+            
+#         im = axarr[ 1+j, i ].imshow( resid )
+#         f.colorbar( im, ax = axarr[ 1+j, i ] ) 
+
+
+cut_strips = 0
+
+
+savepath = '../../../plots_important/point_source/'
+
+titles = [ '240 Pu (moved)', '238 Pu (centered)', '249 Cf (moved)' ]
+
+if cut_strips :
+    title = 'Point Source Fits: With Strip Cuts' 
+    savepath += 'with_strip_cuts.eps'
+    
+else :
+    title = 'Point Source Fits: No Strip Cuts'
+    savepath += 'no_strip_cuts.eps'
+
+
+
+params_guesses = np.array( [ [ 3.0e7, 87.10, -9.31, 58.35 ],
+                               [ 3.0e7, 32.95, -31.45, 57.88 ],
+                               [ 3.0e7, 85.35, -46.06, 57.74 ] ] )
+
+
 f, axarr = plt.subplots( 2, 3, figsize = ( 10, 8 ) ) 
 
+f.suptitle( title ) 
 
-params_guesses = [ np.array( [ [ 3.0e7, 87.10, -9.31, 58.35 ],
-                               [ 3.0e7, 32.95, -31.45, 57.88 ],
-                               [ 3.0e7, 85.35, -46.06, 57.74 ] ] ),
-                   np.array( [ [ 87.10, -9.31, 58.35  ] + [3.0e7] * num_basis_functions,
-                               [ 32.95, -31.45, 57.88 ] + [3.0e7] * num_basis_functions,
-                               [ 85.35, -46.06, 57.74 ] + [3.0e7] * num_basis_functions ] ) ]
-                   
-
-
+f.subplots_adjust( wspace = 0.5 )
 
 for i in range( 3 ) :
 
-    fitfuncs = [ point_source_resid, variable_thickness_resid ]
-    names = [ 'point source calibration', 'variable thickness calibration' ] 
-
-    filter_data( hitmaps[i] )
-    remove_strips( hitmaps[i], [0,31,10,15], [0,31,15,16] ) 
     
-    axarr[0,i].imshow( hitmaps[i] ) 
+    filter_data( hitmaps[i] )
+        
+    if cut_strips : 
+        remove_strips( hitmaps[i], [0,31,10,12,27,15,3], [0,31,15,16,21] ) 
+    
+    # axarr[0,i].imshow( hitmaps[i] )
 
-    print( params_guesses[i] ) 
+    masked_array = np.ma.array( hitmaps[i], mask=np.isnan( hitmaps[i] ) )
+    cmap = matplotlib.cm.jet
+    cmap.set_bad('white',1.)
+    im = axarr[ 0, i ].imshow( hitmaps[i], cmap = cmap  )
+    divider = make_axes_locatable( axarr[0,i] )
+    cax = divider.append_axes("right", size="5%", pad=0.05)
+    f.colorbar(im, cax=cax)
+    
+    # f.colorbar( im, ax = axarr[ 0, i ] ) 
+
+    axarr[0,0].set_ylabel( 'Hit maps' )
+    axarr[1,0].set_ylabel( 'Point Source \nFit Residuals' )
+    axarr[0,i].set_title( titles[i] ) 
 
     hitmap = hitmaps[i]
     d_hitmap = np.sqrt( hitmaps[i] )
     d_hitmap[ d_hitmap == 0 ] = 1
 
-    args = [ ( hitmap, d_hitmap ),
-             ( hitmap, d_hitmap, 1, 5 ) ]
+    args = ( hitmap, d_hitmap )
 
-    
-    for j in range(2) :
+    ret = scipy.optimize.leastsq( point_source_resid, params_guesses[i],
+                                  full_output = 1, args = args )
 
-        print( names[j].upper() )
-
-                                 
-
-        ret = scipy.optimize.leastsq( fitfuncs[j], params_guesses[j],
-                                      full_output = 1, args = args[j]  )
-
-        result, cov, info, mesg, success = ret
-        success = success >= 1
+    result, cov, info, mesg, success = ret
+    success = success >= 1
 
 
-        chisqr = np.sum( info['fvec']**2 )
-        ndata = np.count_nonzero( hitmaps[i][ ( hitmaps[i] != np.nan ) ] ) 
-        nfree =  ndata - len( result ) 
-        redchisqr = chisqr / nfree
+    chisqr = np.sum( info['fvec']**2 )
+    ndata = np.count_nonzero( hitmaps[i][ ~ np.isnan( hitmaps[i] ) ] ) 
+    nfree =  ndata - len( result ) 
+    redchisqr = chisqr / nfree
 
 
-        print( result )
-        print( redchisqr )
-        pvalue = 1 - chi2.cdf( chisqr, nfree )
-        print( 'pvalue: %.2f' % pvalue )
-        print( success )
+    print( 'guess: ' + str( params_guesses[i] ) ) 
+    print( 'result: ' + str( result ) )
+    print( 'redchisqr: ' + str( redchisqr ) )
+    pvalue = 1 - chi2.cdf( chisqr, nfree )
+    print( 'pvalue: %.2f' % pvalue )
+    print( 'fit converged: ' + str( success ) )
 
 
-        if cov is not None :
-            errors = np.sqrt( np.diag( cov ) * redchisqr )
-            print( errors ) 
+    if cov is not None :
+        errors = np.sqrt( np.diag( cov ) * redchisqr )
+        print( 'errors: ', errors ) 
 
-        print( '\n\n' ) 
+    print( '\n\n' ) 
 
-        resid = ( hitmap - point_source_counts( result, hitmap ) ) / d_hitmap 
+    resid = ( hitmap - point_source_counts( result, hitmap ) ) / d_hitmap 
 
-        im = axarr[ 1+j, i ].imshow( resid )
-        f.colorbar( im, ax = axarr[ 1+j, i ] ) 
+    im = axarr[ 1, i ].imshow( resid, cmap = cmap )
+    divider = make_axes_locatable( axarr[1,i] )
+    cax = divider.append_axes("right", size="5%", pad=0.1)
+    f.colorbar(im, cax=cax)
 
-    
+    axarr[1,i].set_title( r'$\tilde \chi^2 = %d / %d = %.2f$'
+                          % ( np.rint( chisqr ), ndata, redchisqr ) )
+
+
+plt.savefig( savepath, format='eps', dpi=1000 )
     
 plt.show() 
